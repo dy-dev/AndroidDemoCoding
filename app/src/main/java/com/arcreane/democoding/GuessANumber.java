dc18e20c94d8e3c36fd4824519bd48d57d41ee1d
arcreaneDev <dominique@arcreane.com>
2021-03-02 15:37:37 +0100

    Correction de l'exercice sur les layout chapitre 3 slide 43

---
+++ app/src/main/java/com/arcreane/democoding/GuessANumber.java
@@ -0,0 +1,14 @@
+package com.arcreane.democoding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuessANumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_a_number);
+    }
+}
